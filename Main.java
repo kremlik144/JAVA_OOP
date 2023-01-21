42d2d70a332f26b8d51700142fc118c42b7f575a
«Sergey» <fomin-sm1998@mail.ru>
2023-01-21 23:42:05 +0300

    Delete HW 5

--- Main.java
+++
@@ -1,9 +0,0 @@
-import Model.AttendanceService;
-import Presenter.Controller;
-import View.AttendanceView;
-
-class Main{
-    public static void main(String[] args) {
-        new Controller(new AttendanceService(239), new AttendanceView()).button();;
-    }
-}
